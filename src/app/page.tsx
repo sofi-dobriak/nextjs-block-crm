6bdd37556f85c65ccfe5bd85773067058e116e1d
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-09 12:48:18 +0300

    module_2_styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/StatusLabel/StatusLabel';
+
 export default function Home() {
   return (
     <main>
-      <h2>Home page</h2>
+      <h1 className="text-4xl font-bold ">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
