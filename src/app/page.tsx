a42a5e1595d3538a9a86c2147fe93ce5292ca2f2
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-09 16:43:15 +0300

    module_04_rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import StatusLabel, { Status } from './components/StatusLabel/StatusLabel';
+import AddCompanyButton from './components/AddCompanyButton';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-4xl font-bold ">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
